6f9cadd80734425b39cd8b09002daf26680a4679
Cedric <cedric.atzori@le-campus-numerique.fr>
2024-01-15 16:13:40 +0100

    TheEnd

--- src/Exo3.java
+++ src/Exo3.java
@@ -1,2 +1,86 @@
-package PACKAGE_NAME;public class Exo3 {
+import java.util.ArrayList;
+
+public class Exo3 {
+
+    public static void main(String[] args) {
+        fibo();
+        //playFiboRecurcive();
+       //approximation_fibo();
+
+        System.out.println(getApproxiamationFibo(0.01F));
+    }
+
+    public static void fibo() {
+        int nbr1 = 0;
+        int nbr2 = 1;
+        int nbr3;
+        int i;
+        int count = 10;
+
+        System.out.print(nbr1 + " " + nbr2);
+
+        for (i = 2; i < count; ++i) {
+            nbr3 = nbr1 + nbr2;
+            nbr1 = nbr2;
+            nbr2 = nbr3;
+            System.out.print(" " + nbr3);
+        }
+    }
+
+    public static Integer fibRecursive(Integer nbr) {
+        if (nbr <= 1) {
+            return nbr;
+        }
+
+        return fibRecursive(nbr - 1) + fibRecursive(nbr - 2);
+    }
+
+    public static void playFiboRecurcive() {
+        int nbr = 10;
+        for (int i = 0; i < nbr; i++) {
+            System.out.print(fibRecursive(i) + " ");
+        }
+    }
+
+
+    public static void approximation_fibo(Float nbr) {
+        Float epsilon = 0.01F;
+        float phi = (float) (1 + Math.sqrt(5)) / 2;
+
+
+    }
+
+    public static ArrayList<Integer> getApproxiamationFibo(float epsilon) {
+
+        float phi = (float) (1 + Math.sqrt(5)) / 2;
+
+        ArrayList<Integer> fibonacciArrayList = new ArrayList<>();
+
+        ArrayList<Integer> fiboNumber = new ArrayList<>();
+
+        fibonacciArrayList.add(0);
+        fibonacciArrayList.add(1);
+
+
+        int i = 2;
+
+        do {
+            fibonacciArrayList.add(i, fibonacciArrayList.get(i - 2) + fibonacciArrayList.get(i - 1));
+
+            if (Math.abs(((float) fibonacciArrayList.get(i - 1) / fibonacciArrayList.get(i - 2)) - phi) <= epsilon) {
+                fiboNumber.add(fibonacciArrayList.get(i - 1));
+                fiboNumber.add(fibonacciArrayList.get(i - 2));
+                return fiboNumber;
+            }
+
+            i++;
+
+        } while (fiboNumber.isEmpty());
+        return fiboNumber;
+    }
+
+
+
+
+
 }
--- src/Exo4.java
+++ src/Exo4.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Exo4 {
+import java.util.ArrayList;
+
+public class Exo4 {
+
+
+    public static void main(String[] args){
+
+        describe();
+        to_upper_case();
+
+    }
+
+    public static void describe() {
+        ArrayList<String> prenom = new ArrayList<String>();
+        prenom.add("cedric");
+        prenom.add("charly");
+        prenom.add("nicolas");
+        prenom.add("jean");
+
+        for (String i : prenom) {
+            if (i.length() > 4) {
+                System.out.println(i + " " + i.length());
+            }
+        }
+    }
+
+    public static void to_upper_case(){
+
+        ArrayList<String> prenom = new ArrayList<String>();
+        prenom.add("cedric");
+        prenom.add("charly");
+        prenom.add("nicolas");
+        prenom.add("jean");
+
+        for (String i : prenom){
+            System.out.println(i.toUpperCase() + " " + i.length());
+
+        }
+
+    }
+
+
+
 }
