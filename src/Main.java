ee614ed3b11815aba8e49ac6f68e9bcd17eecc41
Cedric <cedric.atzori@le-campus-numerique.fr>
2024-01-15 10:05:16 +0100

    2A – Boucles, conditions et fonctions

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+    public static void main(String[] args) {
+        enumerate_while(100);
+        enumerate_for(100);
+        enumerate_even(100);
+    }
+
+    public static void enumerate_while( Integer nbr){
+        int i = 0;
+        while (i <= nbr) {
+            System.out.println("While" + i);
+            i++;
+        }
+    }
+
+    public static void enumerate_for(Integer nbr){
+
+        for (int j = 0; j <= nbr; j++) {
+            System.out.println("For" + j);
+        }
+    }
+
+    public static void enumerate_even(Integer nbr){
+
+        for (int k = 1; k <= nbr; k++) {
+            if (k % 2 == 0) {
+                System.out.println("nombres pairs" + k);
+            }
+        }
+    }
+
+
+}
