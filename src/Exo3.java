2e7b5ffe6b27d73668892cdc58a81aeaaa483ce6
Cedric <cedric.atzori@le-campus-numerique.fr>
2024-01-16 10:55:56 +0100

    TheEnd

--- src/Exo3.java
+++ src/Exo3.java
@@ -3,29 +3,30 @@ import java.util.ArrayList;
 public class Exo3 {
 
     public static void main(String[] args) {
-        fibo();
-        //playFiboRecurcive();
-       //approximation_fibo();
 
-        System.out.println(getApproxiamationFibo(0.01F));
+        //System.out.println(fibo(10));
+        //System.out.println(playFiboRecurcive());
+        //System.out.println(getApproxiamationFibo(0.01F));
+        System.out.println(getApproxiamationFibo(0.01));
     }
 
-    public static void fibo() {
+    public static ArrayList<String> fibo(Integer count) {
         int nbr1 = 0;
         int nbr2 = 1;
         int nbr3;
         int i;
-        int count = 10;
 
-        System.out.print(nbr1 + " " + nbr2);
+        ArrayList<String> fiboListe = new ArrayList<String>();
 
         for (i = 2; i < count; ++i) {
             nbr3 = nbr1 + nbr2;
             nbr1 = nbr2;
             nbr2 = nbr3;
-            System.out.print(" " + nbr3);
+            fiboListe.add(String.valueOf(" " + nbr3));
         }
+        return fiboListe;
     }
+
 
     public static Integer fibRecursive(Integer nbr) {
         if (nbr <= 1) {
@@ -35,52 +36,39 @@ public class Exo3 {
         return fibRecursive(nbr - 1) + fibRecursive(nbr - 2);
     }
 
-    public static void playFiboRecurcive() {
+    public static ArrayList<String> playFiboRecurcive() {
+
+        ArrayList<String> fiboList = new ArrayList<String>();
+
         int nbr = 10;
         for (int i = 0; i < nbr; i++) {
-            System.out.print(fibRecursive(i) + " ");
+
+            fiboList.add(fibRecursive(i) + " ");
         }
+        return fiboList;
     }
 
 
-    public static void approximation_fibo(Float nbr) {
-        Float epsilon = 0.01F;
-        float phi = (float) (1 + Math.sqrt(5)) / 2;
-
-
-    }
-
-    public static ArrayList<Integer> getApproxiamationFibo(float epsilon) {
-
+    public static ArrayList<Integer> getApproxiamationFibo(Double epsilon) {
         float phi = (float) (1 + Math.sqrt(5)) / 2;
 
         ArrayList<Integer> fibonacciArrayList = new ArrayList<>();
-
         ArrayList<Integer> fiboNumber = new ArrayList<>();
 
         fibonacciArrayList.add(0);
         fibonacciArrayList.add(1);
 
-
         int i = 2;
 
-        do {
+        while (Math.abs(((float) fibonacciArrayList.get(i - 1) / fibonacciArrayList.get(i - 2)) - phi) > epsilon){
             fibonacciArrayList.add(i, fibonacciArrayList.get(i - 2) + fibonacciArrayList.get(i - 1));
-
-            if (Math.abs(((float) fibonacciArrayList.get(i - 1) / fibonacciArrayList.get(i - 2)) - phi) <= epsilon) {
-                fiboNumber.add(fibonacciArrayList.get(i - 1));
-                fiboNumber.add(fibonacciArrayList.get(i - 2));
-                return fiboNumber;
-            }
-
             i++;
+        }
 
-        } while (fiboNumber.isEmpty());
+        fiboNumber.add(fibonacciArrayList.get(i - 1));
+        fiboNumber.add(fibonacciArrayList.get(i - 2));
         return fiboNumber;
     }
-
-
-
 
 
 }
